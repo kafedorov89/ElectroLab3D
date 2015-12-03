49468da29ba9198fdc8db573e5444c23b6c0f0fb
pmerzlyakov <pmerzlyakov@rc-online.org>
2015-12-03 19:47:36 +0500

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,78 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CourseFieldAdmin (admin.StackedInline):
+    fieldsets = [
+        (u'', {'fields': ['name', 'course', 'type', 'wp_param']}),
+        (u'Параметры', {'fields': ['param1', 'param2', 'param3', 'number', 'in_course', 'in_report']}),
+        (u'Контроль', {'fields': ['min_right', 'max_right']}),
+    ]
+
+    model = models.CourseField
+    extra = 0
+
+    def get_course (self, obj):
+        return obj.course.name
+    get_course.short_description = u"Лабораторная работа"
+    get_course.admin_order_field = 'course__name'
+
+    list_display = ('get_course', 'name', 'type', 'wp_param', 'number', 'in_course', 'in_report')
+
+
+@admin.register (models.Course)
+class CourseAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (u'Заголовок', {'fields': ['name']}),
+        (u'Параметры', {'fields': ['last_date', 'user', 'duration']}),
+    ]
+
+    list_display = ('name', 'last_date', 'user', 'duration')
+
+    inlines = [CourseFieldAdmin]
+
+
+class AnswerInline (admin.StackedInline):
+    model = models.Answer
+    list_display = ('text_answer', 'right')
+    extra = 0
+
+
+@admin.register (models.Question)
+class QuestionAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (u'', {'fields': ['course', 'text_question', 'number']}),
+    ]
+
+    list_display = ('course', 'text_question', 'number')
+
+    inlines = [AnswerInline]
+
+
+@admin.register (models.Method)
+class MethodAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (u'', {'fields': ['course', 'text_question']}),
+    ]
+
+    list_display = ('course', 'text_question')
+
+
+@admin.register (models.Workplace)
+class WorkplaceAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (u'', {'fields': ['name', 'wp_type', 'url']}),
+    ]
+
+    list_display = ('name', 'wp_type', 'url')
+
+
+@admin.register (models.WpParam)
+class WpParamAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (u'', {'fields': ['workplace', 'name', 'wp_param_type', 'code']}),
+    ]
+
+    list_display = ('workplace', 'name', 'wp_param_type', 'code')
+
--- main/models.py
+++ main/models.py
@@ -6,54 +6,97 @@ from django.contrib.auth.models import User
 class WpType (models.Model):
     name = models.CharField (max_length = 200)
 
+    def __unicode__ (self):
+        return self.name
+
 
 class Workplace (models.Model):
-    name = models.CharField (max_length = 200)
-    wp_type = models.ForeignKey (WpType)
-    url = models.CharField (max_length = 200, default = '')
+    name = models.CharField (max_length = 200, verbose_name = u"Название")
+    wp_type = models.ForeignKey (WpType, verbose_name = u"Тип")
+    url = models.CharField (max_length = 200, default = '', verbose_name = u"Адрес")
+
+    class Meta:
+        verbose_name = u"Стенд"
+        verbose_name_plural = u"Стенды"
+
+    def __unicode__ (self):
+        return u"{} url: {}".format (self.name, self.url)
 
 
 class WpParamType (models.Model):
     name = models.CharField (max_length = 200)
 
+    def __unicode__ (self):
+        return self.name
+
 
 class WpParam (models.Model):
-    name = models.CharField (max_length = 200)
-    wp_param_type = models.ForeignKey (WpParamType)
-    workplace = models.ForeignKey (Workplace)
-    code = models.CharField (max_length = 200, default = '')
+    name = models.CharField (max_length = 200, verbose_name = u"Название")
+    wp_param_type = models.ForeignKey (WpParamType, verbose_name = u"Тип")
+    workplace = models.ForeignKey (Workplace, verbose_name = u"Стенд")
+    code = models.CharField (max_length = 200, default = '', verbose_name = u"Код")
+
+    def __unicode__ (self):
+        return u"{} {} {}".format (self.workplace.name, self.name, self.code)
+
+    class Meta:
+        verbose_name = u"Параметр стенда"
+        verbose_name_plural = u"Параметры стенда"
 
 
 class Course (models.Model):
-    name = models.CharField (max_length = 200)
-    last_date = models.DateTimeField ()
-    user = models.ForeignKey (User)
-    duration = models.DurationField ()
+    name = models.CharField (max_length = 200, verbose_name = u"Название")
+    last_date = models.DateTimeField (verbose_name = u"Дата")
+    user = models.ForeignKey (User, verbose_name = u"Преподаватель")
+    duration = models.DurationField (verbose_name = u"Время на выполнение")
+
+    def __unicode__ (self):
+        return self.name
+
+    class Meta:
+        verbose_name = u"Лабораторная работа"
+        verbose_name_plural = u"Лабораторнаые работы"
+        ordering = ('-name',)
 
 
 class Method (models.Model):
-    course = models.ForeignKey (Course)
-    text_question = models.TextField ()
+    course = models.ForeignKey (Course, verbose_name = u"Лабораторная работа")
+    text_question = models.TextField (verbose_name = u"Текст")
+
+    class Meta:
+        verbose_name = u"Методические материалы"
+        verbose_name_plural = u"Методические материалы"
 
 
 class FieldType (models.Model):
     name = models.CharField (max_length = 200)
     code = models.CharField (max_length = 200, default = '')
 
+    def __unicode__ (self):
+        return self.name
+
 
 class CourseField (models.Model):
-    course = models.ForeignKey (Course, default = 1)
-    wp_param = models.ForeignKey (WpParam, null = True)
-    type = models.ForeignKey (FieldType)
-    name = models.CharField (max_length = 200)
-    param1 = models.TextField (null = True)
-    param2 = models.TextField (null = True)
-    param3 = models.TextField (null = True)
-    number = models.IntegerField (default = 1)
-    in_course = models.BooleanField (default = False)
-    in_report = models.BooleanField (default = False)
-    min_right = models.DecimalField (max_digits = 14, decimal_places = 4, default = 0)
-    max_right = models.DecimalField (max_digits = 14, decimal_places = 4, default = 0)
+    course = models.ForeignKey (Course, default = 1, verbose_name = u"Лабораторная работа")
+    wp_param = models.ForeignKey (WpParam, null = True, verbose_name = u"UID")
+    type = models.ForeignKey (FieldType, verbose_name = u"Тип поля")
+    name = models.CharField (max_length = 200, verbose_name = u"Заголовок")
+    param1 = models.TextField (null = True, verbose_name = u"Параметр 1")
+    param2 = models.TextField (null = True, verbose_name = u"Параметр 2")
+    param3 = models.TextField (null = True, verbose_name = u"Параметр 3")
+    number = models.IntegerField (default = 1, verbose_name = u"Сортировка")
+    in_course = models.BooleanField (default = False, verbose_name = u"Отображение в лабораторной")
+    in_report = models.BooleanField (default = False, verbose_name = u"Отображение в отчете")
+    min_right = models.DecimalField (max_digits = 14, decimal_places = 4, default = 0, verbose_name = u"Минимальное правильное значение")
+    max_right = models.DecimalField (max_digits = 14, decimal_places = 4, default = 0, verbose_name = u"Максимальное правильное значение")
+
+    class Meta:
+        verbose_name = u"Поле лабораторной"
+        verbose_name_plural = u"Поля лабораторных"
+        ordering = ('-course', 'number')
+
+    def __unicode__ (self):
+        return u"{} тип: {} сортировка: {}".format (self.name, self.type.name, self.number)
 
 
 class UserFieldParam (models.Model):
@@ -72,16 +115,27 @@ class UserCourseState (models.Model):
 
 
 class Question (models.Model):
-    course = models.ForeignKey (Course)
-    text_question = models.TextField ()
-    number = models.IntegerField (default = 1)
+    course = models.ForeignKey (Course, verbose_name = u"Лабораторная работа")
+    text_question = models.TextField (verbose_name = u"Текст вопроса")
+    number = models.IntegerField (default = 1, verbose_name = u"Сортировка")
+
+    class Meta:
+        verbose_name = u"Входной контроль"
+        verbose_name_plural = u"Входной контроль"
+        ordering = ('-course', 'number')
 
 
 class Answer (models.Model):
-    question = models.ForeignKey (Question)
-    text_answer = models.TextField ()
-    right = models.BooleanField (default = False)
+    question = models.ForeignKey (Question, verbose_name = u"Вопрос")
+    text_answer = models.TextField (verbose_name = u"Текст ответа")
+    right = models.BooleanField (default = False, verbose_name = u"Является верным")
 
+    class Meta:
+        verbose_name = u"Ответ"
+        verbose_name_plural = u"Ответы"
+
+    def __unicode__ (self):
+        return self.text_answer
 
 class UserAnswer (models.Model):
     user = models.ForeignKey (User)
